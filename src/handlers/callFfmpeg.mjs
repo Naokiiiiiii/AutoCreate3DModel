e560e68df8028779f5a71957ac6bdfbe8b4c158e
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-11-19 14:30:14 +0900

    fix: 命名変更

--- src/handlers/callFfmpeg.mjs
+++ src/handlers/callFfmpeg.mjs
@@ -2,11 +2,11 @@ import AWS from 'aws-sdk'
 const lambda = new AWS.Lambda()
 
 export const callFfmpeg = async (event) => {
-  const fpsList = [1, 3]
+  const fpsList = [1]
   await Promise.all(
     fpsList.map(async (fps) => {
       const params = {
-        FunctionName: process.env.LAMBDA,
+        FunctionName: 'AutoCreate3DModel-ConvertMovieToImageFunction-xInDPNVCZpr5',
         Payload: JSON.stringify({
           bucket_name: event['detail']['bucket']['name'],
           bucket_key: event['detail']['object']['key'],
